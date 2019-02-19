eb5e7d7f880f2d04703d4836294badb9ce9f9ab7
McNultyPT <mcnultypt@gmail.com>
2019-02-19 13:21:03 -0800

    adding a .post method to add a post

--- data/db-router.js
+++ data/db-router.js
@@ -33,4 +33,23 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    const postInfo = req.body;
+
+    if (!postInfo.title || !postInfo.contents)
+        return res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+    });
+
+    try {
+        const post = await Posts.insert(postInfo);
+        res.status(201).json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "There was an error while saving the post to the database"
+        });
+    }
+});
+
 module.exports = router;
