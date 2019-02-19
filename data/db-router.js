f502ba6120bd8b8396de959982c02850e88e48ef
McNultyPT <mcnultypt@gmail.com>
2019-02-19 13:07:48 -0800

    adding .get method to find posts

--- data/db-router.js
+++ data/db-router.js
@@ -4,4 +4,16 @@ const Posts = require('./db.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "The posts information could not be retrieved."
+        });
+    }
+});
+
 module.exports = router;
